dab677ce9fb4fb6a49b947f8000d817b5504a1dc
Amit Kumar <amitgarg.cse@gmail.com>
2020-04-04 15:10:40 +0530

    shell scripting. added documentation

--- git-all.sh
+++ git-all.sh
@@ -9,15 +9,18 @@ if [ "$addStatus" != "0" ]; then
     exit
 fi
 
-
+#commit command
 git commit -m "$1"
+#status check
 addStatus=$?
 if [ "$addStatus" != "0" ]; then
     say "commit command failed"
     exit
 fi
 
+#push command
 git push origin master
+#status check
 addStatus=$?
 if [ "$addStatus" != "0" ]; then
     say "push command failed"
