3d96244d3b0e20606f4f59b0fbc085c42c5e7bf5
Amit Kumar <amitgarg.cse@gmail.com>
2020-04-04 15:04:09 +0530

    shell scripting. corrected var definitions

--- git-all.sh
+++ git-all.sh
@@ -3,23 +3,23 @@
 #add command
 git add --all
 #status check
-addStatus $?
-if addStatus!=0; then
+addStatus=$?
+if $addStatus!=0; then
     say "add command failed"
     exit
 fi
 
 
 git commit -m "$1"
-addStatus $?
-if addStatus!=0; then
+addStatus=$?
+if $addStatus!=0; then
     say "commit command failed"
     exit
 fi
 
 git push origin master
-addStatus $?
-if addStatus!=0; then
+addStatus=$?
+if $addStatus!=0; then
     say "push command failed"
     exit
 fi
