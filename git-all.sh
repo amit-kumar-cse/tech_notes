5d8da4b83a9eeb9ba2b0a633452ad16ea1b48607
Amit Kumar <amitgarg.cse@gmail.com>
2020-04-04 14:58:15 +0530

    shell scripting. added condition and command status check

--- git-all.sh
+++ git-all.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#add command
 git add --all
+#status check
+addStatus $?
+if addStatus!=0; then
+    say "add command failed"
+    exit
+else
+    say "add command passed"
+fi
+
+
 git commit -m "$1"
-git push origin master
+if(git push origin master)
+
+#improvements to do. if any of these commands fail. show an error message and say "checkin failed"
