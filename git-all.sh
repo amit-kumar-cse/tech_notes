9c17b3e7d7e73246d46d9432c724e8f46042bfed
Amit Kumar <amitgarg.cse@gmail.com>
2020-04-04 15:00:58 +0530

    shell scripting. completed condition and command status check for git push script

--- git-all.sh
+++ git-all.sh
@@ -4,15 +4,22 @@
 git add --all
 #status check
 addStatus $?
-if addStatus==0; then
-    say "add command passed"
-else
+if addStatus!=0; then
     say "add command failed"
     exit
 fi
 
 
 git commit -m "$1"
-if(git push origin master)
+addStatus $?
+if addStatus!=0; then
+    say "commit command failed"
+    exit
+fi
 
-#improvements to do. if any of these commands fail. show an error message and say "checkin failed"
+git push origin master
+addStatus $?
+if addStatus!=0; then
+    say "push command failed"
+    exit
+fi
