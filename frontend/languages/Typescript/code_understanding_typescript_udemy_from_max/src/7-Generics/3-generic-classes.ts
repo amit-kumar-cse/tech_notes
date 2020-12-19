1b5a4e82c8325f6ff56a56e071d8fe7f95c40c41
Amit Kumar <amitgarg.cse@gmail.com>
2020-12-19 17:02:56 +0530

    Generic classes and utility types

---
+++ frontend/languages/Typescript/code_understanding_typescript_udemy_from_max/src/7-Generics/3-generic-classes.ts
@@ -0,0 +1,55 @@
+{
+    class DataStorage<T> {
+        private data: T[] = [];
+
+        addItem(item: T) {
+            this.data.push(item);
+        }
+
+        removeItem(item: T) {
+            if(this.data.indexOf(item) !== -1)  {
+                return;
+            }
+            this.data.splice(this.data.indexOf(item), 1);
+        }
+
+        getItems() {
+            return [...this.data];
+        }
+    }
+
+    const textStorage = new DataStorage<string>();
+    textStorage.addItem('Hare');
+    textStorage.addItem('Krsna');
+    textStorage.addItem('Rama');
+    console.log(textStorage.getItems());
+
+    const numberStorage = new DataStorage<number>();
+}
+
+//Utility types examples of Partial and Readonly.
+{
+    interface CourseGoal {
+        title: string;
+        description: string;
+        completeUntil: Date;
+    }
+
+    function createCourseGoal(
+        title: string,
+        description: string,
+        completeUntil: Date
+    ): CourseGoal {
+        const courseGoal: Partial<CourseGoal> = {};
+        courseGoal.title = title;
+        courseGoal.description = description;
+        courseGoal.completeUntil = completeUntil;
+
+        return courseGoal as CourseGoal;
+    };
+
+    //readonly example
+    const names: Readonly<string[]> = ['Hare', 'Krsna'];
+    // names.push('Rama');
+    // names[0] = 'Govind';
+}
---
+++ frontend/languages/Typescript/understanding-typescript-udemy-course-from-max/7. Generics/3-generic-classes-and-utility-types.md
@@ -0,0 +1,3 @@
+# Generic classes and Utility Types
+
+[code](../../code_understanding_typescript_udemy_from_max/src/7-Generics/3-generic-classes.ts)
